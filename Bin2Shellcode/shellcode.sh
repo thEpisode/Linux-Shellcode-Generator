b7a0a1ce604eb63c976a99738f8cac4f6efed70d
Cami Rodriguez <camiepisode@outlook.com>
2017-08-11 04:07:23 -0500

    flags for test

--- Bin2Shellcode/shellcode.sh
+++ Bin2Shellcode/shellcode.sh
@@ -3,11 +3,18 @@
 binaryFile=$1
 saveInFile=$2
 
+echo $binaryFile
+echo $saveInFile
+
+for i in $(objdump -d $binaryFile |grep "^ " |cut -f2); do echo -n '\x'$i; done;
 
 # Get data from binary
 for i in $(objdump -d $binaryFile |grep "^ " |cut -f2);do
     shellcode=$shellcode'\x'$i
 done
+
+echo 'Shellcode:'
+echo $shellcode
 
 while test $# -gt 0; do
     case saveInFile in 
