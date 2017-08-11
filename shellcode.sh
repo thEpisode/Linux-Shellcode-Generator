13e3796947fe917c418ef5a9f476b52a4c10a412
Cami Rodriguez <camiepisode@outlook.com>
2017-08-11 14:43:05 -0500

    corrected bug

--- shellcode.sh
+++ shellcode.sh
@@ -9,6 +9,37 @@ for i in $(objdump -d $binaryFile |grep "^ " |cut -f2);do
     shellcode=$shellcode'\x'$i
 done
 
+function saveShellcode {
+    rm -f shellcode.txt
+    echo $shellcode >> $binaryFile"_shellcode.txt"
+}
+
+function injectShellcode {
+    shellcodeTestFile=$currentFolder"/ShellcodeTest/shellcode.c"
+    shellcodeTestLine="1"
+    shellcode_escaped=$( echo "$shellcode" | sed -e 's/[\/&]/\\&/g' )
+    code="char code[] =\"$shellcode_escaped\";"
+
+    sed -i "${shellcodeTestLine}s/.*/$code/" "$shellcodeTestFile"
+}
+
+function buildShellcode {
+    gcc -o $currentFolder"/ShellcodeTest/shellcode" $currentFolder"/ShellcodeTest/shellcode.c"
+}
+
+function runShellcode {
+    ./ShellcodeTest/shellcode
+}
+
+# Print in console
+function printShellcode {
+    echo
+    echo 'Shellcode extracted: '
+    echo
+    echo $shellcode
+    echo
+}
+
 while true; do
     case "$saveInFile" in 
         # Save shellcode into a .txt file
@@ -57,34 +88,3 @@ while true; do
         ;;
     esac
 done
-
-function saveShellcode {
-    rm -f shellcode.txt
-    echo $shellcode >> $binaryFile"_shellcode.txt"
-}
-
-function injectShellcode {
-    shellcodeTestFile=$currentFolder"/ShellcodeTest/shellcode.c"
-    shellcodeTestLine="1"
-    shellcode_escaped=$( echo "$shellcode" | sed -e 's/[\/&]/\\&/g' )
-    code="char code[] =\"$shellcode_escaped\";"
-
-    sed -i "${shellcodeTestLine}s/.*/$code/" "$shellcodeTestFile"
-}
-
-function buildShellcode {
-    gcc -o $currentFolder"/ShellcodeTest/shellcode" $currentFolder"/ShellcodeTest/shellcode.c"
-}
-
-function runShellcode {
-    ./ShellcodeTest/shellcode
-}
-
-# Print in console
-function printShellcode {
-    echo
-    echo 'Shellcode extracted: '
-    echo
-    echo $shellcode
-    echo
-}
