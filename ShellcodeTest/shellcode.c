d9e4e0e37eb74c6def54f954f577d98459640183
Cami Rodriguez <camiepisode@outlook.com>
2017-08-11 15:29:27 -0500

    reverted main

--- ShellcodeTest/shellcode.c
+++ ShellcodeTest/shellcode.c
@@ -3,10 +3,10 @@
 
 char code[] ="\xb8\x04\x00\x00\x00\xbb\x01\x00\x00\x00\xb9\xa0\x90\x04\x08\xba\x1c\x00\x00\x00\xcd\x80\xb8\x01\x00\x00\x00\xcd\x80";
 
-main()
+int main(int argc, char *argv[])
 {
     printf("Shellcode Length:  %d\n", (int)strlen(code));
-    
+
     int (*ret)() = (int(*)())code;
     ret();
 }
