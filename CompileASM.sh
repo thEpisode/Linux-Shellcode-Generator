e6c4ea26186dfddd3183b13d9498718babd29067
Cami Rodriguez <camiepisode@outlook.com>
2017-08-11 12:52:34 -0500

    changed folder dir

--- CompileASM.sh
+++ CompileASM.sh
@@ -7,7 +7,7 @@ currentFolder=${PWD}
 while true; do
     case "$projectFlag" in 
         -f|--folder)
-            cd '~/ASM/'$projectFolder'/'
+            cd "$currentFolder/$projectFolder/"
             nasm -f elf $projectFolder'.asm'
             ld -o $projectFolder $projectFolder'.o'
             cd $currentFolder
