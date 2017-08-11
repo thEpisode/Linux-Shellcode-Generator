b245591381014931e5aeecede80ce1ff15f9f1b0
Cami Rodriguez <camiepisode@outlook.com>
2017-08-11 16:29:21 -0500

    changed name and included new flag

---
+++ ASMFastDevelopment.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+flag=$1
+projectFolder=$2
+currentFolder=${PWD}
+
+function newProject {
+    mkdir "$currentFolder/$projectFolder/$projectFolder.asm"
+    echo "; hello.asm" >> "$currentFolder/$projectFolder/$projectFolder.asm"
+    echo "; x86 architecture" >> "$currentFolder/$projectFolder/$projectFolder.asm"
+    echo "; Intel specification" >> "$currentFolder/$projectFolder/$projectFolder.asm"
+    echo "section .data" >> "$currentFolder/$projectFolder/$projectFolder.asm"
+    echo "  " >> "$currentFolder/$projectFolder/$projectFolder.asm"
+    echo "" >> "$currentFolder/$projectFolder/$projectFolder.asm"
+    echo "section .text" >> "$currentFolder/$projectFolder/$projectFolder.asm"
+    echo "    global _start" >> "$currentFolder/$projectFolder/$projectFolder.asm"
+    echo "" >> "$currentFolder/$projectFolder/$projectFolder.asm"
+    echo "_start:" >> "$currentFolder/$projectFolder/$projectFolder.asm"
+    echo "  " >> "$currentFolder/$projectFolder/$projectFolder.asm"
+    echo "  " >> "$currentFolder/$projectFolder/$projectFolder.asm"
+    echo "  " >> "$currentFolder/$projectFolder/$projectFolder.asm"
+}
+
+function compile {
+    cd "$currentFolder/$projectFolder/"
+    nasm -f elf32 $projectFolder'.asm' -o $projectFolder'.o'
+    ld -m elf_i386 $projectFolder'.o' -o $projectFolder
+    cd $currentFolder
+}
+
+function printHelp {
+    echo
+    echo 'Usage: bash CompileASM.sh [<options>] [<folder name>]'
+    echo
+    echo '    -h, --help                Show helps'
+    echo '    -n, --new                 Create new project with folder name given'
+    echo '    -c, --compile             Compile .asm file inside folder'
+    echo '        Disclaimer: This script works in current folder, do not try to compile outside this folder'
+    echo
+    echo 'Example:'
+    echo '  > bash CompileASM.sh -f HelloWorld'
+    echo
+}
+
+function printFinish {    
+    echo 'Compilation done with 0 errors'
+    echo 'Check $projectFolder folder'
+    echo
+}
+
+while true; do
+    case "$flag" in
+        -h|--help)
+            printHelp
+            break
+        ;;
+        -n|--new)
+            newProject 
+            break
+        ;;
+        -c|--compile)
+            compile
+            printFinish
+            break
+        ;;
+        *)
+            printHelp
+            break
+        ;;
+    esac
+done
