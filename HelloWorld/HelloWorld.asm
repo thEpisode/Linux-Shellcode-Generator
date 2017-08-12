f06fabf95cd19a9c1eb44a486514ce5cfa3c3e4f
Cami Rodriguez <camiepisode@outlook.com>
2017-08-12 00:29:19 -0500

    avoiding nulls

--- HelloWorld/HelloWorld.asm
+++ HelloWorld/HelloWorld.asm
@@ -11,11 +11,20 @@ section .text
 
 _start:
 
-    mov eax, 4  ; ssize_t sys_write(unsigned int fd, const char * buf, size_t count) (write in console)
-    mov ebx, 1  ; sys_write argv #1 (unsigned int)
-    mov ecx, msg; sys_write argv #2 (char *)
-    mov edx, len; sys_write argv #3 (size_t)
-    int 0x80    ; interrumpt system (execute)
+    xor eax, eax    ;clean up the registers to avoid generating nulls
+    xor ebx, ebx
+    xor edx, edx
+    xor ecx, ecx
 
-    mov eax, 1 ; sys_exit
+    ; Using 4 bits registers because 8 bits are unused
+
+    mov al, 4  ; ssize_t sys_write(unsigned int fd, const char * buf, size_t count) (write in console)
+    mov bl, 1  ; sys_write argv #1 (unsigned int)
+    mov cl, msg; sys_write argv #2 (char *)
+    mov dl, len; sys_write argv #3 (size_t)
+    int 0x80   ; interrumpt system (execute)
+
+    xor eax, eax
+    mov al, 1   sys_exit
+    xor ebx,ebx
     int 0x80
