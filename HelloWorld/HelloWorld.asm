57f61cba62cae1bcdc79afcc94320324db786a65
Cami Rodriguez <camiepisode@outlook.com>
2017-08-12 23:55:40 -0500

    Changed doc

--- HelloWorld/HelloWorld.asm
+++ HelloWorld/HelloWorld.asm
@@ -12,10 +12,10 @@ section .text
 _start:
 
     mov eax, 4  ; ssize_t sys_write(unsigned int fd, const char * buf, size_t count) (write in console)
-    mov ebx, 1  ; sys_write argv #1 (unsigned int)
-    mov ecx, msg; sys_write argv #2 (char *)
-    mov edx, len; sys_write argv #3 (size_t)
-    int 0x80    ; interrumpt system (execute)
+    mov ebx, 1  ; sys_write argv #1 (unsigned int), file descriptor (stdout)
+    mov ecx, msg; sys_write argv #2 (char *), message to write
+    mov edx, len; sys_write argv #3 (size_t), message length
+    int 0x80    ; interrumpt system (call kernel)
 
-    mov eax, 1 ; sys_exit
-    int 0x80
+    mov eax, 1  ; sys_exit
+    int 0x80    ; call kernel
