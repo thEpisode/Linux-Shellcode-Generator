f49b30738cc04984266b82194c6398c68a94f264
Cami Rodriguez <camiepisode@outlook.com>
2017-08-16 13:06:16 -0500

    Printing 9 stars

--- ShowStars/ShowStars.asm
+++ ShowStars/ShowStars.asm
@@ -2,12 +2,25 @@
 ; x86 architecture
 ; Intel specification
 section .data
-  
+    msg db 'Displaying 9 stars',0xa ;a message
+    len equ $ - msg  ;length of message
+    s2 times 9 db '*'
 
 section .text
     global _start
 
 _start:
-  
-  
-  
+    mov	edx,len  ;message length
+    mov	ecx,msg  ;message to write
+    mov	ebx,1    ;file descriptor (stdout)
+    mov	eax,4    ;system call number (sys_write)
+    int	0x80     ;call kernel
+        
+    mov	edx,9    ;message length
+    mov	ecx,s2   ;message to write
+    mov	ebx,1    ;file descriptor (stdout)
+    mov	eax,4    ;system call number (sys_write)
+    int	0x80     ;call kernel
+        
+    mov	eax,1    ;system call number (sys_exit)
+    int	0x80     ;call kernel
