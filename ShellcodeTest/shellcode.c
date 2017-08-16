64e78621f2b1d2fbb717fff70d1dee3f23794350
thEpisode <camiepisode@outlook.com>
2017-08-16 10:04:16 -0800

    Added new project

--- ShellcodeTest/shellcode.c
+++ ShellcodeTest/shellcode.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 #include<string.h>
 
-unsigned char code[] ="\xb8\x04\x00\x00\x00\xbb\x01\x00\x00\x00\xb9\xa0\x90\x04\x08\xba\x1c\x00\x00\x00\xcd\x80\xb8\x01\x00\x00\x00\xcd\x80";
+char code[] ="\x31\xc0\x31\xdb\x31\xd2\x31\xc9\xb0\x04\xb3\x01\xb9\xa0\x90\x04\x08\xba\x1c\x00\x00\x00\xcd\x80\x31\xc0\xb0\x01\x31\xdb\xcd\x80";
 
 int main(int argc, char *argv[])
 {
     printf("Shellcode Length:  %d\n", (int)strlen(code));
+	
+	int (*func)();
+	func = (int (*)()) code;
+	(int)(*func)();
 
-    int (*ret)() = (int(*)())code;
-    ret();
+    //int (*ret)() = (int(*)())code;
+    //ret();
 }
---
+++ ShowStars/ShowStars.asm
@@ -0,0 +1,13 @@
+; hello.asm
+; x86 architecture
+; Intel specification
+section .data
+  
+
+section .text
+    global _start
+
+_start:
+  
+  
+  
